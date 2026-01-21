73cd2f7efede4051abfe6c29aab2f201b290ff8f
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-01-21 19:11:30 +0100

    Initial commit.

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("com.diffplug.spotless") version "8.1.0"
+
+    application
+    // jacoco
+//    alias(libs.plugins.spotless)
+//    alias(libs.plugins.lombok)
+//    alias(libs.plugins.shadow)
+//    alias(libs.plugins.sonarqube)
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
