98e5f1420dbb280d62b4a8348b49350f8da4aa77
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-01-28 18:54:34 +0100

    Fix deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.53.0"
     id("com.diffplug.spotless") version "8.1.0"
     id("org.sonarqube") version "7.1.0.6387"
@@ -21,16 +20,6 @@ application { mainClass.set("hexlet.code.App") }
 repositories {
     mavenCentral()
     gradlePluginPortal()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.getByName("run", JavaExec::class) {
